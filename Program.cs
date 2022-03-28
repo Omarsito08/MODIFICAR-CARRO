41adad52383c412e06e77bdd66935dbf8102d3bb
Omarsito08 <79231800+Omarsito08@users.noreply.github.com>
2022-03-27 21:21:11 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+using MODIFICAR_CARRO.CLASES;
+
+MITROQUITA TROKA = new MITROQUITA("HONDA CIVIC", 2011, "AZUL", 160);
+
+
+
+Console.WriteLine($"Tu nave es: {TROKA.Marca} modelo {TROKA.Modelo}");
+TROKA.EncenderMotor();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.acelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.desacelerar();
+TROKA.Disminuir();
+TROKA.parqueado();
+TROKA.Pipiiiiit();
